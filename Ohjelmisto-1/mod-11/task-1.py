1bd8f9e8768620303ed8e4a396a3097d36be42ec
Elias Ojala <91126885+ravend3v@users.noreply.github.com>
2024-08-27 09:21:14 +0300

    Module 11 fixed

--- Ohjelmisto-1/mod-11/task-1.py
+++ Ohjelmisto-1/mod-11/task-1.py
@@ -1,30 +1,32 @@
 class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
 
-    class Kirja:
-        def __init__(self, nimi, kirjoittaja, sivumaara):
-            self.nimi = nimi
-            self.kirjoittaja = kirjoittaja
-            self.sivumaara = sivumaara
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumaara):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
 
-        def tulosta_tiedot(self):
-            print(f"Kirja: {self.nimi}\nKirjoittaja: {self.kirjoittaja}\nSivumäärä: {self.sivumaara}")
 
-    class Lehti:
-        def __init__(self, nimi, paatoimittaja):
-            self.nimi = nimi
-            self.paatoimittaja = paatoimittaja
+    def tulosta_tiedot(self):
+        print(f"{self.nimi}, kirjoittanut {self.kirjoittaja}, {self.sivumaara} sivua")
 
-        def tulosta_tiedot(self):
-            print(f"Lehti: {self.nimi}\nPäätoimittaja: {self.paatoimittaja}")
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja = paatoimittaja
 
+    def tulosta_tiedot(self):
+        print(f"{self.nimi}, päätoimittaja {self.paatoimittaja}")
 
 def main():
-    kirja = Julkaisu.Kirja("Hytti n:o 6", "Rosa Liksom", 200)
-    lehti = Julkaisu.Lehti("Aku Ankka", "Aki Hyyppä")
+    kirja = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+    lehti = Lehti("Aku Ankka", "Aki Hyyppä")
 
-    lehti.tulosta_tiedot()
-    print()
     kirja.tulosta_tiedot()
+    lehti.tulosta_tiedot()
 
 if __name__ == "__main__":
     main()
+
